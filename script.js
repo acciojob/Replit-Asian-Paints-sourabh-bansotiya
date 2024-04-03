2cf95bce59064f3a9139ce2eb9ad187954e38b71
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-03 14:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let changeColor = document.getElementById('change_button');
+
+changeColor.addEventListener('click', ()=>{
+	const blockId = document.getElementById('block_id').value;
+	const colorId = document.getElementById('color_id').value;
+	
+	for (let i = 1; i <= 9; i++) {
+		const gridItem = document.getElementById(i.toString());
+		
+		if (gridItem && gridItem.id == blockId) {
+			gridItem.style.backgroundColor = colorId;
+		}else{
+			gridItem.style.backgroundColor = 'transparent'
+		}
+	}
+});
+
+let resetColor = document.getElementById('reset');
+resetColor.addEventListener('click', () => {
+	for (let i = 1; i <= 9; i++) {
+		const gridItems = document.getElementById(i.toString());
+		gridItems.style.backgroundColor = 'transparent';
+	}
+});
